f98664a58ae65d6402d8c848a0edccfb649bd779
Michael Gordon <mike1gordon@gmail.com>
2015-06-28 15:07:21 +0200

    First commit - pages + bootstrap

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,22 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def arte
+  end
+
+  def musica
+  end
+
+  def danza
+  end
+
+  def cine
+  end
+
+  def eventos
+  end
+
+  def noticias
+  end
+end
