ec1fd533f6207cfc07d3d00e12ee9d91add34df2
Michael Gordon <mike1gordon@gmail.com>
2015-06-28 19:06:15 +0200

    AWS configured with paperclip

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,9 @@
 class Article < ActiveRecord::Base
 	has_attached_file :image
+
+	validates_attachment_content_type :image, 
+	content_type:  /^image\/(png|gif|jpeg)/,
+	message: "Only images allowed"
+
+	validates :image, attachment_presence: true
 end
